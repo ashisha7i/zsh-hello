f02f6b579436a32e2673694e2584c4d5f0888261
Ashish Agnihotri <aagnihotri@mgmresorts.com>
2025-08-27 13:32:07 -0700

    Basic plugin

---
+++ hello.plugin.zsh
@@ -0,0 +1,9 @@
+# hello.plugin.zsh
+# A sample zcomet plugin that adds a hello_world function
+
+hello_world() {
+  echo "Hello, world from ${0:A:h}!"
+}
+
+# You can also define aliases
+alias hw='hello_world'
