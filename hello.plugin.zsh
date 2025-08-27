cbd256cc825640240f2d63f698de350935a2fe17
Ashish Agnihotri <aagnihotri@mgmresorts.com>
2025-08-27 13:44:13 -0700

    Added completions

--- hello.plugin.zsh
+++ hello.plugin.zsh
@@ -10,3 +10,13 @@ alias hw='hello_world'
 
 # Add this plugin's completions to fpath
 fpath+=("${0:A:h}/completions")
+
+# Initialize completions if not already loaded
+if ! typeset -f compinit >/dev/null; then
+  autoload -Uz compinit
+fi
+
+# Run compinit only if completion system not set up
+if ! whence -w _complete >/dev/null 2>&1; then
+  compinit -i
+fi
